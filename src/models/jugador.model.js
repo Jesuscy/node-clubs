909d212fe79c653f769e68c04fd77c312bd3d330
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-03-03 18:35:16 +0100

    Mod Add jugador.model.js

---
+++ src/models/jugador.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const jugadorSchema = new mongoose.Schema({
+
+    equipoId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Equipo',  // Referencia al modelo de Equipo
+        required: true,
+    },
+
+    jugadorNombre: {
+        type: String,
+        required: true,
+    },
+
+    jugadorEdad: {
+        type: Number,
+        required: true,
+    },
+
+    jugadorPosicion: {
+        type: String,
+        ref: 'Contributor',
+        required: true,
+
+    },
+
+    jugadorAltura: {
+        type: Number,
+        required: true,
+
+    },
+    jugadorPeso: {
+        type: Number,
+        required: true,
+    },
+
+    jugadorGoles: {
+        type: Number,
+    }
+});
+
+const Jugador = mongoose.model('Jugador', jugadorSchema);
+
+module.exports = Jugador;
