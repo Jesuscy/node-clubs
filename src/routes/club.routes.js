e64028a3398ffa6332db36def336853fa16aa143
Luis rojas <luis.alberto.rojas@bootcamp-upgrade.com>
2024-03-04 19:30:46 +0100

    add routes

---
+++ src/routes/club.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+//EL ROUTER ES EL OBJETO QUE GUARDA TODAS LAS RUTAS.
+const clubRouter = express.Router();
+//INSTANCIAMOS AL CONTROLADOR PARA USAR LAS FUNCIONES RELATIVAS A CADA RUTA
+const { getClub, getClubs } = require("../controller/club.controller");
+
+//OBTENER UN CLUB
+clubRouter.get("/:id", getClub);
+
+//OBTENER TODOS LOS CLUBS
+clubRouter.get("/", getClubs);
+
+module.exports = clubRouter;
